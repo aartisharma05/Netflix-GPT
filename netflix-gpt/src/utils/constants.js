038ce94ea3154538967e7cfb3b61f69914c4cc12
aartisharma05 <aartisharma.official1@gmail.com>
2024-01-10 11:50:42 +0530

    logo

--- netflix-gpt/src/utils/constants.js
+++ netflix-gpt/src/utils/constants.js
@@ -2,8 +2,8 @@
 
 
 export const LOGO =
-  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
-
+  // "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+"../cineinsight.png";
   export const USER_AVATAR =
     "https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
 
