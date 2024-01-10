31316506706459a6a755b2d0779a3241629acd4b
aartisharma05 <aartisharma.official1@gmail.com>
2024-01-10 11:50:29 +0530

    design

--- netflix-gpt/src/components/GptMovieSuggestions.js
+++ netflix-gpt/src/components/GptMovieSuggestions.js
@@ -6,7 +6,7 @@ const GptMovieSuggestions = () => {
   if (!movieNames) return null;
 
   return (
-    <div className="p-4 m-4  bg-black text-white bg-opacity-90 rounded-lg">
+    <div className="p-4 m-4 bg-black text-white bg-opacity-90 rounded-lg">
       <div>
         {movieNames.map((movieName, index) => (
           <MovieList
